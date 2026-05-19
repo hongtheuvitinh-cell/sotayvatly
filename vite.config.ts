a8588ae51fd491524072773de3bb1354b18ded93
hongtheuvitinh-cell <hongtheuvitinh@gmail.com>
2026-05-20 06:30:55 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    optimizeDeps: {
+      include: ['rehype-raw', 'react-markdown', 'remark-math', 'rehype-katex'],
+    },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
